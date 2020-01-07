bf02f8349e64b462c12a97e3b36afadf53a5b788
Jocelyn-Miller <joeymmiller@gmail.com>
2020-01-07 12:15:06 -0500

    added tilt

--- Cloud Jump/GameScene.swift
+++ Cloud Jump/GameScene.swift
@@ -5,40 +5,53 @@
 //  Created by  on 1/2/20.
 //  Copyright © 2020 goodstuff. All rights reserved.
 //
-
+import CoreMotion
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene
+class GameScene: SKScene, SKPhysicsContactDelegate
 {
     var bird = SKSpriteNode()
     let cam = SKCameraNode()
-    //var cloud1 = SKSpriteNode()
+    var motionManager: CMMotionManager!
+    var cloud1 = SKSpriteNode()
 
 //player.run(SKAction.applyImpulse(CGVector(dx: 100, dy: 20), duration: 0))
     override func didMove(to view: SKView)
     {
-//        let background = SKSpriteNode(imageNamed: "sky")
-//        background.position = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
-//        background.zPosition = -100
-//        background.size = CGSize(width: view.frame.width, height: view.frame.height)
-//        addChild(background)
-        //create contact body 
-        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
-        borderBody.friction = 0.0
-        self.physicsBody = borderBody
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates()
+        
+        physicsWorld.contactDelegate = self
+
         bird = self.childNode(withName: "bird") as! SKSpriteNode
         bird.texture = SKTexture(imageNamed: "bird")
+        cloud1 = self.childNode(withName: "cloud1") as! SKSpriteNode
+        cloud1.texture = SKTexture(imageNamed: "cloud")
+        cloud1.physicsBody?.categoryBitMask = 2
+        bird.physicsBody?.contactTestBitMask = 2 | 10
+        //bird is 1 clouds are 2 bottom is 10
+        
         self.camera = cam
         cam.setScale(1)
+        bird.physicsBody?.categoryBitMask = 1
+        
+        
         //cam.setScale(0.)
+        //        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        //        borderBody.friction = 0.0
+        //        self.physicsBody = borderBody
 
        
     }
     
     override func update(_ currentTime: CFTimeInterval)
     { //test to see if bird hits the bottom
-        
+        if let accelerometerData = motionManager.accelerometerData {
+            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.x, dy: accelerometerData.acceleration.y)
+            
+        }
+        //cam.position.y = bird.position.y
         var positionx = bird.position.x
         var positiony = bird.position.y
         if positiony < 650
@@ -50,6 +63,17 @@ class GameScene: SKScene
         cam.position.x = frame.width/2
         cam.position.y = bird.position.y
         }
+    }
+    func didBegin(_ contact: SKPhysicsContact)
+    {
+        //accelerate on clouds
+        //stop on bottom
+        if (contact.bodyA.categoryBitMask == 1 && contact.bodyB.categoryBitMask == 2) ||  (contact.bodyA.categoryBitMask == 2 && contact.bodyB.categoryBitMask == 1)
+        {
+            print("hit the cloud")
+            //bird.physicsBody?.velocity = CGVector(dx: 0, dy: 700)
+        }
+        
     }
 
     
