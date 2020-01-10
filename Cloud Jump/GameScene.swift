1f25e43d38998a2a0860c6ee9b6674986894a950
Jocelyn-Miller <joeymmiller@gmail.com>
2020-01-10 12:13:10 -0500

    updated as of friday 1/10/20

--- Cloud Jump/GameScene.swift
+++ Cloud Jump/GameScene.swift
@@ -15,10 +15,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     let cam = SKCameraNode()
     var motionManager: CMMotionManager!
     var cloud1 = SKSpriteNode()
+    //var rand = Int.random(in: 50..<700)
+    var rand = 403
+    var positionYVar = 160
 
 //player.run(SKAction.applyImpulse(CGVector(dx: 100, dy: 20), duration: 0))
     override func didMove(to view: SKView)
     {
+        addClouds()
         motionManager = CMMotionManager()
         motionManager.startAccelerometerUpdates()
         
@@ -28,13 +32,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         bird.texture = SKTexture(imageNamed: "bird")
         cloud1 = self.childNode(withName: "cloud1") as! SKSpriteNode
         cloud1.texture = SKTexture(imageNamed: "cloud")
+        
+        //bird is 1 clouds are 2 bottom is 10
+     
+        self.camera = cam
+        cam.setScale(0.5)
+        bird.physicsBody?.categoryBitMask = 1
         cloud1.physicsBody?.categoryBitMask = 2
         bird.physicsBody?.contactTestBitMask = 2 | 10
-        //bird is 1 clouds are 2 bottom is 10
-        
-        self.camera = cam
-        cam.setScale(1)
-        bird.physicsBody?.categoryBitMask = 1
         
         
         //cam.setScale(0.)
@@ -47,16 +52,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     
     override func update(_ currentTime: CFTimeInterval)
     { //test to see if bird hits the bottom
-        if let accelerometerData = motionManager.accelerometerData {
-            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.x, dy: accelerometerData.acceleration.y)
-            
-        }
-        //cam.position.y = bird.position.y
-        var positionx = bird.position.x
-        var positiony = bird.position.y
-        if positiony < 650
+       if let accelerometerData = motionManager.accelerometerData
+       {
+           physicsWorld.gravity.dx = CGFloat(accelerometerData.acceleration.x * 20)
+       }
+        if bird.position.x > frame.width
         {
-            cam.position.y = 650
+            bird.position.x = 0
+        }
+        if bird.position.x < 0
+        {
+            bird.position.x = frame.width
+        }
+        
+        //cam.position.y = bird.position.y
+        
+        var positiony = bird.position.y
+        if positiony < 400
+        {
+            cam.position.y = 400
             cam.position.x = frame.width / 2
         }
         else{
@@ -71,9 +85,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         if (contact.bodyA.categoryBitMask == 1 && contact.bodyB.categoryBitMask == 2) ||  (contact.bodyA.categoryBitMask == 2 && contact.bodyB.categoryBitMask == 1)
         {
             print("hit the cloud")
-            //bird.physicsBody?.velocity = CGVector(dx: 0, dy: 700)
+            //contact.bodyA.applyImpulse(CGVector(dx: 0, dy: 1000))
+            //contact.bodyB.applyImpulse(CGVector(dx: 0, dy: 1000))
         }
         
+    }
+    func createClouds()
+    {
+        let cloud = SKSpriteNode(imageNamed: "cloud")
+        cloud.position = CGPoint(x: rand, y: positionYVar)
+        cloud.physicsBody?.isDynamic = false
+        
+//        rect.physicsBody = SKPhysicsBody(rectangleOf: rect.frame.size)
+
+        cloud.physicsBody?.affectedByGravity = false
+        cloud.physicsBody?.categoryBitMask = 2
+            cloud.name = "cloud"
+//        let sandwichTexture = SKTexture(imageNamed: "sandwich")
+//        rect.texture = sandwichTexture
+//        //shape.strokeColor = .black
+        addChild(cloud)
+    }
+    func addClouds()
+    {
+        createClouds()
     }
 
     
