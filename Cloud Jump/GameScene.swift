162c3fb00c2c1e8f27b87c006605854d913344fb
Jocelyn-Miller <joeymmiller@gmail.com>
2020-01-21 12:14:35 -0500

    nest shouldn't be active until bird is above it - SCREENSHOTS, LIST OF FEATURES, COMPRESS AND TURN IN

--- Cloud Jump/GameScene.swift
+++ Cloud Jump/GameScene.swift
@@ -20,10 +20,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     var positionYVar = 160
     let bottom = SKSpriteNode(color: UIColor.red, size: CGSize(width: 750,           height: 10))
     let cloudTexture: SKTexture = SKTexture(imageNamed: "cloud")
+    let nestTexture: SKTexture = SKTexture(imageNamed: "nest")
     
     var bounceCounter = 0
+    var cloudCounter = 0
     var cloudCollection: [SKSpriteNode] = []
     var bottomHitBool = false
+    let cloud1 = SKSpriteNode(color: UIColor.white, size: CGSize(width: 100,           height: 40))
+    var nestCollection: [SKSpriteNode] = []
     //var emptyDoubles: [Double] = []
     ///add nest
     ///add stop functions from  breakout
@@ -39,7 +43,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
        bird.texture = SKTexture(imageNamed: "bird")
        bird.physicsBody?.categoryBitMask = 1
        //cloud1.physicsBody?.categoryBitMask = 7
-       bird.physicsBody?.contactTestBitMask = 2 | 11
+       bird.physicsBody?.contactTestBitMask = 2 | 11 | 62
     }
 
 //player.run(SKAction.applyImpulse(CGVector(dx: 100, dy: 20), duration: 0))
@@ -58,6 +62,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         
             //cloud.name = "cloud"
             addChild(cloud1)
+        //cloudCollection.append(cloud1)
         addClouds()
         let groundTexture: SKTexture = SKTexture(imageNamed: "ground")
         bottom.physicsBody = SKPhysicsBody(texture: groundTexture, size: CGSize(width: 750, height: 10))
@@ -98,6 +103,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     
     override func update(_ currentTime: CFTimeInterval)
     { //test to see if bird hits the bottom
+        for cloud in cloudCollection
+        {
+            if cloud.position.y < bird.position.y - 600
+            {
+                //cloud1.removeFromParent()
+                cloud.removeFromParent()
+                
+            }
+        }
         
         ///LETS BIRD ONLY HIT ON THE DOWN BOUNCE
         
@@ -107,6 +121,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate
                 for cloud in cloudCollection
                 {
                 cloud.physicsBody = nil
+                }
+                for nest in nestCollection
+                {
+                    nest.physicsBody = nil
                 }
             }
             else
@@ -120,7 +138,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate
                     cloud.physicsBody?.categoryBitMask = 2
                     cloud.physicsBody?.restitution = 1.2
                 }
-                    
+                for nest in nestCollection
+                {
+                    nest.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 40))
+                    nest.texture = nestTexture
+                    nest.position = CGPoint(x: rand, y: positionYVar)
+                    nest.physicsBody?.isDynamic = false
+                    nest.physicsBody?.affectedByGravity = false
+                    nest.physicsBody?.categoryBitMask = 62
+                    nest.physicsBody?.restitution = 1
+                }
             }
         if bottomHitBool == false
         {
@@ -130,13 +157,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate
             }
         }
         
-        if bird.position.x > frame.width
+        if bird.position.x > frame.width - 230
         {
-            bird.position.x = 0
+            bird.position.x = 100
         }
-        if bird.position.x < 0
+        if bird.position.x < 100
         {
-            bird.position.x = frame.width
+            bird.position.x = frame.width - 230
         }
         
         let positiony = bird.position.y
@@ -148,6 +175,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         else{
         cam.position.x = frame.width/2
         cam.position.y = bird.position.y
+            cloud1.removeFromParent()
         }
     }
     func didBegin(_ contact: SKPhysicsContact)
@@ -156,20 +184,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         //stop on bottom
         if (contact.bodyA.categoryBitMask == 1 && contact.bodyB.categoryBitMask == 2) ||  (contact.bodyA.categoryBitMask == 2 && contact.bodyB.categoryBitMask == 1)
         {
-            print("hit the cloud")
-            if bounceCounter <= 1 {
-            bird.physicsBody?.velocity.dy = (bird.physicsBody?.velocity.dy)! + 400
-                //bounceCounter += 1
-                //player.run(SKAction.applyImpulse(CGVector(dx: 100, dy: 20), duration: 0))
+            print("hit cloud")
+            if bounceCounter >= 1 {
+            bird.physicsBody?.velocity.dy = (bird.physicsBody?.velocity.dy)! + 500
+                bounceCounter += 1
+            //bird.run(SKAction.applyImpulse(CGVector(dx: 0, dy: 1000), duration: 0.1))
         }
             
         }
         if (contact.bodyA.categoryBitMask == 1 && contact.bodyB.categoryBitMask == 11) ||  (contact.bodyA.categoryBitMask == 11 && contact.bodyB.categoryBitMask == 1)
         {
-            print("hit the bottom")
+            print("hit bottom")
+//            let feather =  SKEmitterNode(fileNamed: "feather.sks")
+//
+//            feather?.position = contact.contactPoint
+//            feather?.targetNode = self
+//            addChild(feather!)
             //ADD STOP FUNCTIONS
             resetBird()
-            //motionManager = nil
+            //emittor node
+        }
+        if (contact.bodyA.categoryBitMask == 1 && contact.bodyB.categoryBitMask == 62) ||  (contact.bodyA.categoryBitMask == 62 && contact.bodyB.categoryBitMask == 1)
+        {
+            print("hit nest")
+            bird.removeFromParent()
+            //emittor node
+            
         }
         
         
@@ -177,10 +217,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     
     func createClouds()
     {
-        //cloud.physicsBody = SKPhysicsBody(texture: cloudTexture, size: CGSize(width: 100, height: 40))
-        //let cloudTexture: SKTexture = SKTexture(imageNamed: "cloud")
+
         let cloud = SKSpriteNode(color: UIColor.white, size: CGSize(width: 100,           height: 40))
-        //cloud.physicsBody = SKPhysicsBody(texture: cloudTexture, size: CGSize(width: 100, height: 40))
+
         cloud.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 40))
         cloud.texture = cloudTexture
         cloud.position = CGPoint(x: rand, y: positionYVar)
@@ -196,6 +235,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     {
         for col in 0...10
         {
+        
         positionYVar+=110
             
         var randCheck = rand
@@ -205,6 +245,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate
                 rand = Int.random(in: 240...520)
             }
         createClouds()
+            cloudCounter+=1
+            if cloudCounter == 11
+            {
+                positionYVar += 130
+                rand = Int.random(in: 240...520)
+                while rand >= (randCheck - 50) && rand <= (randCheck+50)
+                {
+                    rand = Int.random(in: 240...520)
+                }
+                createNest()
+            }
            
         }
 
@@ -228,12 +279,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate
            run(sequence)
        }
 
-       
+       func createNest()
+       {
+
+           let nest = SKSpriteNode(color: UIColor.white, size: CGSize(width: 100,           height: 40))
+
+           nest.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 40))
+           nest.texture = nestTexture
+           nest.position = CGPoint(x: rand, y: positionYVar)
+           nest.physicsBody?.isDynamic = false
+           nest.physicsBody?.affectedByGravity = false
+           nest.physicsBody?.categoryBitMask = 62
+           nest.physicsBody?.restitution = 1
+               //cloud.name = "cloud"
+               addChild(nest)
+        nestCollection.append(nest)
+        //cloudCollection.append(nest)
+
+       }
 
 
        
-//    let spark =  SKEmitterNode(fileNamed: "spark.sks")
-//               spark?.position = contact.contactPoint
-//               spark?.targetNode = self
-//               addChild(spark!)
+//    let feather =  SKEmitterNode(fileNamed: "feather.sks")
+//               feather?.position = contact.contactPoint
+//               feather?.targetNode = self
+//               addChild(feather!)
 }
